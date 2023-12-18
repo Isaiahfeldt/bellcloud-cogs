92770225a214d4ddec58566dd3466e11ca2bb17e
Isaiah Feldt <isaiahfeldt@gmail.com>
2023-12-17 23:54:29 -0600

    Create 'emote/cog.py' with AGPL license and type hints
    This commit adds a new 'emote/cog.py' file that brings in the AGPL license, establishes type hints for better code readability, and handles exceptions when importing 'Red', a third-party bot. Additionally, the code file contains organization of bot functionalities using classes and subclasses, enhancing modularity and maintainability.

---
+++ emote/cog.py
@@ -0,0 +1,49 @@
+#  Copyright (c) 2023, Isaiah Feldt
+#  ͏
+#     - This program is free software: you can redistribute it and/or modify it
+#     - under the terms of the GNU Affero General Public License (AGPL) as published by
+#     - the Free Software Foundation, either version 3 of this License,
+#     - or (at your option) any later version.
+#  ͏
+#     - This program is distributed in the hope that it will be useful,
+#     - but without any warranty, without even the implied warranty of
+#     - merchantability or fitness for a particular purpose.
+#     - See the GNU Affero General Public License for more details.
+#  ͏
+#     - You should have received a copy of the GNU Affero General Public License
+#     - If not, please see <https://www.gnu.org/licenses/#GPL>.
+
+from typing import Any, Literal
+
+import discord
+from discord import AppCommandType
+from redbot.core import Config
+from redbot.core.i18n import Translator, cog_i18n
+
+from type_hints.bot import DISCORD_BOT_TYPE
+
+from emote.slash_commands import SlashCommands
+
+_ = Translator("Emote", __file__)
+
+
+@cog_i18n(_)
+class Emotes(
+    # UserCommands,
+    # HybridCommands,
+    SlashCommands,
+    # ContextMenus,
+):
+    """
+    Discord emotes slash commands.
+    Sorta like an emoji, but bigger.
+    """
+
+    __version__ = "0.0.1"
+
+    def __init__(self, bot: DISCORD_BOT_TYPE, *args: Any, **kwargs: Any):
+        super().__init__(*args, **kwargs)
+        self.bot = bot
+
+    async def cog_unload(self) -> None:
+        self.bot.tree.remove_command(self.add_as_emote, type=AppCommandType.message)
