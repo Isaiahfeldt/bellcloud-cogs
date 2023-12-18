de2a00048019ae2210bfe7fb1464c6a220812442
Isaiah Feldt <isaiahfeldt@gmail.com>
2023-12-17 23:54:03 -0600

    Update license to AGPL and improve emote handling
    This commit updates the license from GPL to AGPL and enhances the functionality of emote handling by using Discord's slash command feature. Additionally, the setup function's type hinting has been improved for clarity.

--- emote/__init__.py
+++ emote/__init__.py
@@ -1,22 +1,25 @@
 #  Copyright (c) 2023, Isaiah Feldt
+#  ͏
 #     - This program is free software: you can redistribute it and/or modify it
-#     - under the terms of the GNU General Public License (GPL) as published by
+#     - under the terms of the GNU Affero General Public License (AGPL) as published by
 #     - the Free Software Foundation, either version 3 of this License,
 #     - or (at your option) any later version.
 #  ͏
 #     - This program is distributed in the hope that it will be useful,
 #     - but without any warranty, without even the implied warranty of
 #     - merchantability or fitness for a particular purpose.
-#     - See the GNU General Public License for more details.
+#     - See the GNU Affero General Public License for more details.
 #  ͏
-#     - You should have received a copy of the GNU GPL along with this program.
+#     - You should have received a copy of the GNU Affero General Public License
 #     - If not, please see <https://www.gnu.org/licenses/#GPL>.
 
-from redbot.core.bot import Red
+from redbot.core.utils import get_end_user_data_statement
+
+from type_hints.bot import DISCORD_BOT_TYPE
 
 from .cog import Emotes
 
 
-async def setup(bot: Red) -> None:
+async def setup(bot: DISCORD_BOT_TYPE) -> None:
     cog = Emotes(bot)
     await bot.add_cog(cog)
