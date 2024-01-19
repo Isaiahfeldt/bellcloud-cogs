b1ab96627216fda8e53f5b88cb9f8a42220485c0
Isaiah Feldt <isaiahfeldt@gmail.com>
2024-01-18 22:20:52 -0600

    Refactor emote handling and introduce Emote data class
    Introduced a data class, Emote, to better handle and represent emote data in pipeline.py. Updated the database management method "get_emote" in database.py to return a complete asyncpg.Record object instead of just the file path. Improved the emote handling in slash_commands.py by using this new Emote class. Updated relevant comments and docstrings for clarity.

--- emote/slash_commands.py
+++ emote/slash_commands.py
@@ -155,6 +155,5 @@ class SlashCommands(commands.Cog):
         pipeline_verbose = ""
 
         emote = await db.get_emote(emote_name, False)
-        for i in emote:
-            await message.channel.send(str(i))
+        await message.channel.send(str(emote))
         await send_emote(message, emote, pipeline_verbose)
