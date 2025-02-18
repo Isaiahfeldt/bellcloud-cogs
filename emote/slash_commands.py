918075b5d5117f64a1a0fbcaa6fb60ec5c706831
Isaiah <isaiahfeldt@gmail.com>
2025-02-18 17:28:02 -0600

    "Comment out debug code
    Disable debug messages related to emote cache status by commenting out the relevant code block to streamline responses."

--- emote/slash_commands.py
+++ emote/slash_commands.py
@@ -39,11 +39,11 @@ def calculate_extra_args(time_elapsed, emote) -> list:
     extra_args = []
     if SlashCommands.latency_enabled:
         extra_args.append(f"Your request was processed in `{time_elapsed}` seconds!")
-    if SlashCommands.debug_enabled:
-        if SlashCommands.was_cached:
-            extra_args.append(f"The emote `{emote.name}` was found in cache.")
-        else:
-            extra_args.append(f"The emote `{emote.name}` was not found in cache.")
+    # if SlashCommands.debug_enabled:
+    #     if SlashCommands.was_cached:
+    #         extra_args.append(f"The emote `{emote.name}` was found in cache.")
+    #     else:
+    #         extra_args.append(f"The emote `{emote.name}` was not found in cache.")
     # Append emote.notes if it exists (i.e. not None)
     # if emote.notes:
     #     extra_args.append(emote.notes)
