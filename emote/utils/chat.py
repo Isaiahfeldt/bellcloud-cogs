83549a3a029129d23f79bd80c43655a20ef610f3
Isaiah <isaiahfeldt@gmail.com>
2025-03-27 15:46:10 -0500

    Refactor error handling in effect_wrapper for improved clarity

--- emote/utils/chat.py
+++ emote/utils/chat.py
@@ -139,8 +139,8 @@ async def send_followup_embed(message: discord.Message, emote):
     embed = discord.Embed(title="Umm, just a follow up...", colour=EmbedColor.GREY.value)
     for key, followup_msg in emote.followup.items():
         embed.add_field(name=f"{key}", value=followup_msg, inline=False)
-    embed.set_footer(text="This embed will delete automatically in 10 seconds.")
-    await message.channel.send(embed=embed, delete_after=10)
+    embed.set_footer(text="This embed will delete automatically in 15 seconds.")
+    await message.channel.send(embed=embed, delete_after=15)
 
 
 async def generate_token(interaction: discord.Interaction):
