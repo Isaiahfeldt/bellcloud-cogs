c5584582acb05cf9d4bde08237de2e030d305357
Isaiah Feldt <isaiahfeldt@gmail.com>
2025-03-29 18:32:46 -0500

    Add guild-only restriction to on_message listener
    Enforces the `on_message` listener to operate only within guilds using the `@commands.guild_only()` decorator. Additionally, refactors an if-condition for better readability when ignoring owner commands.

--- emote/slash_commands.py
+++ emote/slash_commands.py
@@ -442,14 +442,14 @@ class SlashCommands(commands.Cog):
         )
 
     @commands.Cog.listener()
+    @commands.guild_only()
     async def on_message(self, message: discord.Message):
         if message.author.bot:
             return
 
         # Check if message is in the 'general-3-uwu' channel
         if message.channel.name.lower() == "general-3-uwu" or message.channel.name.lower() == "general-3":
-            if not message.author.id == 138148168360656896 and message.content.startswith(
-                    "!"):  # Ignore owner using commands
+            if not (message.author.id == 138148168360656896 and message.content.startswith("!")):  # Ignore owner
                 if not is_enclosed_in_colon(message):  # Ignore :emotes:
                     await message.channel.typing()
                     await self.handle_april_fools(message)
