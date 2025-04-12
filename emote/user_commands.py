435ed0779cd90984484842f33b46ca1e4fe3f339
Isaiah <isaiahfeldt@gmail.com>
2025-04-12 10:40:25 -0500

    Testing User Commands

--- emote/user_commands.py
+++ emote/user_commands.py
@@ -9,8 +9,8 @@ _ = Translator("Emote", __file__)
 @cog_i18n(_)
 class UserCommands(commands.Cog):
 
-    @app_commands.command()
+    @app_commands.command(name="effect", description="Adds effects to images")
     @app_commands.user_install()
-    @app_commands.allowed_contexts(guilds=False, dms=True, private_channels=False)
+    @app_commands.allowed_contexts(guilds=False, dms=True, private_channels=True)
     async def effect(self, interaction: discord.Interaction) -> None:
         await interaction.response.send_message('I am installed in users by default!')
