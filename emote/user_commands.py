791368814211c27810fe9baf14ccef8abb513423
Isaiah <isaiahfeldt@gmail.com>
2025-04-12 10:23:51 -0500

    Testing User Commands

--- emote/user_commands.py
+++ emote/user_commands.py
@@ -12,12 +12,4 @@ class UserCommands(commands.Cog):
     @app_commands.command()
     @app_commands.user_install()
     async def effect(self, interaction: discord.Interaction) -> None:
-        message = interaction.message
-
-        # image_attachment = None
-        # for attachment in message.attachments:
-        #     if attachment.filename.lower().endswith(('.png', '.jpg', '.jpeg', '.gif', '.webp', '.mp4')):
-        #         image_attachment = attachment
-        #         break
-
-        await message.reply(content=f"This is a test! {message.content}", mention_author=False)
+        await interaction.response.send_message('I am installed in users by default!')
